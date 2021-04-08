3faf2c459a08f85bbfbcc92ed30a6032bb08ba63
だいら <kanikama44@gmail.com>
2021-04-08 20:53:32 +0900

    Check2

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,23 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+	private static String firstName = "一";
+	private static String lastName = "平";
+
+	public static void main(String[] args) {
+
+		// インスタンス生成
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		// メソッド実行
+		printName();
+		pet.introduce();
+		robotPet.introduce();
+	}
+
+	private static void printName() {
+		System.out.println("prinrNameメソッド →" + firstName + lastName);
+	}
+}
