9bce934278c7f1038f26b29a9c8b6fb24be0ab93
だいら <kanikama44@gmail.com>
2021-04-12 16:13:26 +0900

    check2 修正

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -12,12 +12,12 @@ public class Check {
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		// メソッド実行
-		printName();
+		printName(firstName,lastName);
 		pet.introduce();
 		robotPet.introduce();
 	}
 
-	private static void printName() {
+	private static void printName(String firstName, String lastName) {
 		System.out.println("prinrNameメソッド →" + firstName + lastName);
 	}
 }
